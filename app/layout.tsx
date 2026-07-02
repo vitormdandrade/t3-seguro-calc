9d46a521af9353e92eb9c91d7e51c6d77141e58b
vitormdandrade <vitormachadodeandrade@gmail.com>
2026-07-02 07:44:58 -0300

    revert: remove custom analytics tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/next";
-import { AnalyticsTracker } from "@/components/AnalyticsTracker";
 import MobileNav from "./components/MobileNav";
 import "./globals.css";
 
@@ -136,7 +135,6 @@ export default function RootLayout({
           </div>
         </footer>
         <Analytics />
-        <AnalyticsTracker />
       </body>
     </html>
   );
